9832ed0f2790bfd83ca47e18fa0f981352cf9066
GAMAL <jamalbadie24@gmail.com>
2026-01-20 10:15:00 +0100

    Modified menu choices

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,9 @@ public class Main {
            try {
             System.out.println("\n---- Main Menu ----");
             System.out.println("1. Add a client");
-            System.out.println("2. Create a bank account");
-            System.out.println("3. Display Accounts");
-            System.out.println("4. Check balance");
+            System.out.println("2. Create a new bank account");
+            System.out.println("3. Display All Accounts");
+            System.out.println("4. Check balance of an account");
             System.out.println("5. Deposit money");
             System.out.println("6. Withdraw money");
             System.out.println("7. Delete account");
